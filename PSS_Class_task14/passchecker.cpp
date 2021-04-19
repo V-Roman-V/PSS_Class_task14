0fa2d273cedb353cd2fcc06129bf361c42e88c93
V_Roman_V <vor.roman.v@gmail.com>
2021-04-19 21:47:39 +0300

    Initial commit

---
+++ PSS_Class_task14/passchecker.cpp
@@ -0,0 +1,6 @@
+#include "passchecker.h"
+
+PassChecker::PassChecker()
+{
+
+}
---
+++ PSS_Class_task14/passchecker.h
@@ -0,0 +1,13 @@
+#ifndef PASSWORDCHECKER_H
+#define PASSWORDCHECKER_H
+
+
+class PassChecker
+{
+private:
+    PassChecker();
+public:
+
+};
+
+#endif // PASSWORDCHECKER_H
